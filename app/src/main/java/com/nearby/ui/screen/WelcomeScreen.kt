af932ba874d0c0b84cac2fc9a8f0a8df0b38ed04
Marco <marco@crabki.com>
2024-12-11 17:13:27 -0300

    feat: Add welcome screen

--- app/src/main/java/com/nearby/ui/screen/WelcomeScreen.kt
+++ app/src/main/java/com/nearby/ui/screen/WelcomeScreen.kt
@@ -1,2 +1,45 @@
 package com.nearby.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nearby.ui.comonents.button.NearbyButton
+import com.nearby.ui.comonents.welcome.WelcomeContent
+import com.nearby.ui.comonents.welcome.WelcomeHeader
+
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .background(Color.White)
+            .fillMaxSize()
+            .padding(horizontal = 40.dp, vertical = 48.dp)
+            .verticalScroll(state = rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        WelcomeHeader()
+        WelcomeContent()
+        NearbyButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Começar",
+            onClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun WelcomeScreenPreview() {
+    WelcomeScreen()
+}
