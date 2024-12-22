0698a81b93d3cbd6cd7c3b0c703951f02cc9c9d4
Marco Aurélio <sgtmarcoaurelio@icloud.com>
2024-12-22 10:17:43 -0300

    :truck: separate GoogleMap into a component

--- app/src/main/java/com/nearby/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/nearby/ui/screen/home/HomeScreen.kt
@@ -10,41 +10,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.drawable.toBitmap
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.rememberCameraPositionState
-import com.nearby.R
 import com.nearby.data.model.Market
-import com.nearby.data.model.mock.mockUserLocation
 import com.nearby.ui.comonents.category.NearbyCategoryFilterChipList
+import com.nearby.ui.comonents.home.NearbyGoogleMap
 import com.nearby.ui.comonents.market.NearbyMarketCardList
 import com.nearby.ui.theme.Gray100
-import com.nearby.ui.util.findNortheastPoint
-import com.nearby.ui.util.findSouthwestPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import okhttp3.internal.toImmutableList
-import kotlin.collections.orEmpty
-import kotlin.collections.plus
-import kotlin.math.roundToInt
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -110,92 +85,6 @@ fun HomeScreen(
         }
     )
 
-}
-
-@Composable
-fun NearbyGoogleMap(modifier: Modifier = Modifier, uiState: HomeUiState) {
-    val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
-    val density = LocalDensity.current
-
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(mockUserLocation, 13f)
-    }
-    val uiSettings by remember {
-        mutableStateOf(MapUiSettings(zoomControlsEnabled = true))
-    }
-
-    GoogleMap(
-        modifier = modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState,
-        uiSettings = uiSettings
-    ) {
-        context.getDrawable(R.drawable.ic_user_location)?.let {
-            Marker(
-                state = MarkerState(position = mockUserLocation),
-                icon = BitmapDescriptorFactory.fromBitmap(
-                    it.toBitmap(
-                        width = density.run { 72.dp.toPx() }.roundToInt(),
-                        height = density.run { 72.dp.toPx() }.roundToInt(),
-                    )
-                )
-            )
-        }
-
-        if (!uiState.markets.isNullOrEmpty()) {
-            context.getDrawable(R.drawable.img_pin)?.let {
-                uiState.marketLocations?.toImmutableList()
-                    ?.forEachIndexed { index, location ->
-                        Marker(
-                            state = MarkerState(position = location),
-                            icon = BitmapDescriptorFactory.fromBitmap(
-                                it.toBitmap(
-                                    width = density.run { 36.dp.toPx() }
-                                        .roundToInt(),
-                                    height = density.run { 36.dp.toPx() }
-                                        .roundToInt(),
-                                )
-                            ),
-                            title = uiState.markets[
-                                index
-                            ].name
-                        )
-                    }.also {
-                        coroutineScope.launch {
-
-                            val allMarks = uiState.marketLocations?.plus(
-                                mockUserLocation
-                            )
-                            val southwestPoint =
-                                findSouthwestPoint(points = allMarks.orEmpty())
-                            val northeastPoint =
-                                findNortheastPoint(points = allMarks.orEmpty())
-                            val centerLatitudePoint =
-                                (southwestPoint.latitude + northeastPoint.latitude) / 2
-                            val centerLongitudePoint =
-                                (southwestPoint.longitude + northeastPoint.longitude) / 2
-                            val cameraUpdate =
-                                CameraUpdateFactory.newCameraPosition(
-                                    CameraPosition(
-                                        LatLng(
-                                            centerLatitudePoint,
-                                            centerLongitudePoint,
-                                        ),
-                                        13f,
-                                        0f,
-                                        0f
-                                    )
-                                )
-                            delay(200)
-                            cameraPositionState.animate(
-                                cameraUpdate,
-                                durationMs = 500
-                            )
-                        }
-                    }
-            }
-        }
-    }
 }
 
 @Preview
